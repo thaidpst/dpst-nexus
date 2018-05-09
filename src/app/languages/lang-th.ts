29fc2f743aa8b711f9d6ac1b4df52957637ca2cd
Chin Isradisaikul <chinawat@cs.cornell.edu>
2018-05-09 10:27:28 +0700

    More translations.

--- src/app/languages/lang-th.ts
+++ src/app/languages/lang-th.ts
@@ -1,22 +1,62 @@
 export const LANG_TH_TRANS = {
+  '.':
+    '',
+  '+66 2 381 0750':
+    '02 381 0750',
+  '+66 2 392 4021':
+    '02 392 4021',
+  '5 letters minimum':
+    'อย่างต่ำ 5 ตัวอักษร',
   'about':
     'เกี่ยวกับ',
   'about application':
     'เกี่ยวกับ Application',
+  'about you':
+    'เกี่ยวกับคุณ',
   'address':
     'ที่อยู่',
+  'after we received your message, we will reply to you within 2 business days. thank you!':
+    'หลังจากที่เราได้รับข้อความของคุณแล้ว เราจะติดต่อกลับภายใน 2 วันทำการ',
+  'call us':
+    'โทรหาเรา',
+  'cancel':
+    'ยกเลิก',
+  'confirm password':
+    'ยืนยันรหัสผ่าน',
   'contact us':
     'ติดต่อเรา',
+  'country':
+    'ประเทศ',
+  'delete':
+    'ลบ',
+  'delete a user':
+    'ลบบัญชีผู้ใช้',
   'dpst #':
     'พสวท. รุ่นที่',
   'education':
     'ระดับการศึกษา',
   'e-mail':
     'อีเมล',
+  'e-mail address':
+    'ที่อยู่อีเมล',
+  'e-mail is already in use':
+    'อีเมลนี้ได้ถูกใช้งานแล้ว',
+  'e-mail us':
+    'อีเมลหาเรา',
+  'edit profile':
+    'แก้ไขประวัติส่วนตัว',
+  'fax us':
+    'แฟกซ์หาเรา',
+  'first name':
+    'ชื่อจริง',
   'forgot':
     'ลืม',
   'forms':
     'แบบฟอร์ม',
+  'get in touch':
+    'ติดต่อเรา',
+  'go back':
+    'ย้อนกลับ',
   'hello,':
     'สวัสดี',
   'history':
@@ -25,22 +65,40 @@ export const LANG_TH_TRANS = {
     'หน้าแรก',
   'how to use':
     'วิธีใช้งาน',
+  'ipst<br>924 sukhumvit rd<br>phra khanong, khlong toei<br>bangkok 10110 thailand':
+    'สสวท.<br>924 ถนนสุขุมวิท<br>แขวงพระโขนง เขตคลองเตย<br>กรุงเทพมหานคร 10110',
+  'if you have any concerns, please ':
+    'หากท่านมีคำถามหรือข้อสงสัย กรุณา',
+  'last name':
+    'นามสกุล',
   'log in':
     'เข้าสู่ระบบ',
   'log out':
     'ออกจากระบบ',
   'major':
-    'สาขาวิชา',
+    'วิชาเอก',
+  'meet us':
+    'พบปะเรา',
+  'message':
+    'ข้อความ',
+  'message us now':
+    'ส่งข้อความหาเราตอนนี้',
   'mission':
     'เป้าหมาย',
   'password':
     'รหัสผ่าน',
+  'password does not match':
+    'รหัสผ่านไม่ตรงกัน',
+  'password is too short':
+    'รหัสผ่านสั้นเกินไป',
   'phone':
     'โทรศัพท์',
   'position':
     'ตำเเหน่ง',
   'profile':
     'ประวัติส่วนตัว',
+  'province/state':
+    'จังหวัด/รัฐ',
   'register':
     'สมัครสมาชิก',
   'register to our community':
@@ -49,10 +107,24 @@ export const LANG_TH_TRANS = {
     'จำฉันไว้ในระบบ',
   'school/university':
     'สถานศีกษา',
+  'settings':
+    'ตั้งค่า',
+  'send message':
+    'ส่งข้อความ',
   'status':
     'สถานะ',
+  'update your profile':
+    'บันทึกประวัติส่วนตัว',
+  'used for logging in':
+    'ใช้ในการเข้าสู่ระบบ',
+  'user menu':
+    'เมนูผู้ใช้งาน',
+  'user panel':
+    'หน้าผู้ใช้งาน',
   'username':
     'ชื่อผู้ใช้งาน',
+  'username is already in use':
+    'ชื่อผู้ใช้งานนี้ได้ถูกใช้งานแล้ว',
   'welcome to dpst database manager':
     'ยินดีต้อนรับเข้าสู่ระบบจัดการข้อมูลของ พสวท.',
   'what is this?':
@@ -61,10 +133,40 @@ export const LANG_TH_TRANS = {
     'สถานที่ทำงาน',
   'wrong username or password':
     'ชื่อผู้ใช้งานหรือรหัสผ่านผิด',
+  'you are about to ':
+    'คุณกำลังจะ',
+  'your account has been approved, and you can use our service.':
+    'บัญชีของท่านได้รับการยืนยันเรียบร้อยแล้ว และท่านสามารถใช้บริการของเราได้',
+  'your account has been created. please wait for our administrators to approve your account before you can use our service.':
+    'คุณได้ลงทะเบียนในระบบของเราเป็นที่เรียบร้อยแล้ว กรุณารอเราตรวจสอบข้อมูล ก่อนที่ท่านจะเริ่มใช้บริการได้',
+  'your account is currently banned.':
+    'บัญชีของท่านได้ถูกระงับการใช้งานชั่วคราว',
+  'your address':
+    'ที่อยู่ของคุณ',
+  'your address line 2':
+    'ที่อยู่ของคุณ บรรทัดที่ 2',
+  'your dpst #':
+    'รุ่น พสวท. ของคุณ',
   'your e-mail address':
     'อีเมลของคุณ',
+  'your education':
+    'ระดับการศึกษาของคุณ',
+  'your field of study':
+    'สาขาวิชาของคุณ',
+  'your line id':
+    'Line ID ของคุณ',
   'your name':
-    'ชื่อของคุณ'
+    'ชื่อของคุณ',
+  'your position':
+    'ตำแหน่งของคุณ',
+  'your school/university':
+    'สถานศึกษาของคุณ',
+  'your telephone number':
+    'หมายเลขโทรศัพท์ของคุณ',
+  'your workplace':
+    'สถานที่ทำงานของคุณ',
+  'zip code':
+    'รหัสไปรษณีย์'
 };
 
 // {{'text' | translate:settingService.getLanguage() }}
